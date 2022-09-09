a0e2de091404ad9abc436d840027f8dcd621ea07
Laurent <selvanadin.laurent.sio@gmail.com>
2022-09-09 15:52:52 +0200

    Ajoutez des fichiers projet.

---
+++ Compte/Banque.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Compte
+{
+    class Banque
+    {
+        public Banque()
+        {
+            mesComptes = new List<Compte>();
+        }
+        
+        private List<Compte> mesComptes;
+
+    private List<Compte> MesComptes
+        {
+            get { return mesComptes; }
+            set { mesComptes = value; }
+        }
+
+        public Compte Compte { get; private set; }
+
+        public void ajouterCompte(int leNumero, string lenom,double leSolde,double leDecouvertSolde)
+        {
+
+
+            Compte = new Compte(leNumero, lenom, leSolde, leDecouvertSolde);
+       
+
+
+
+
+
+
+
+        }
+    }
+}
---
+++ Compte/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Compte
+{
+    class Program
+    {
+        static void Main()
+        {
+            Compte c1, c2;
+            c1 = new Compte(12345, "toto", 1000, -500);
+            c2 = new Compte(45657, "titi", 2000, -1000);
+            if (c1.superieur(c2))
+                Console.WriteLine("superieur");
+            else
+                Console.WriteLine("inférieur");
+        }
+    }
+}
